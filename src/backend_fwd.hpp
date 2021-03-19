d67672706c344a24e8b48ce10c0dec5682b6a53e
bonachea <dobonachea@lbl.gov>
2021-03-18 20:20:17 -0700

    tweak UPCXX_INTERNAL_ONLY to include "detail" in the symbol name

--- src/backend_fwd.hpp
+++ src/backend_fwd.hpp
@@ -114,7 +114,7 @@ namespace upcxx {
     #define UPCXX_CONCAT_(a, b) a ## b
     #define UPCXX_CONCAT(a, b) UPCXX_CONCAT_(a, b)
     // Macro for members that are intended to be private.
-    #define UPCXX_INTERNAL_ONLY(name) UPCXX_CONCAT(private_do_not_use_, name)
+    #define UPCXX_INTERNAL_ONLY(name) UPCXX_CONCAT(private_detail_do_not_use_, name)
   }
     
   class persona;
