47265f32d03607dbd9e1cf980ed0a4f1f08a0116
Amir Kamil <akamil@umich.edu>
2021-04-04 01:56:51 -0400

    Excise unused during_user and during_level.

--- src/backend_fwd.hpp
+++ src/backend_fwd.hpp
@@ -267,12 +267,6 @@ namespace backend {
   void quiesce(const team &tm, entry_barrier eb);
 
   void warn_collective_in_progress(const char *fnname, entry_barrier eb=entry_barrier::none);
-  
-  template<progress_level level, typename Fn>
-  void during_level(Fn &&fn, persona &active_per = current_persona());
-  
-  template<typename Fn>
-  void during_user(Fn &&fn, persona &active_per = current_persona());
 
   /* fulfill_during: enlists a promise to be fulfilled in the given persona's
    * lpc queue. Since persona headers are lpc's and thus store queue linkage
