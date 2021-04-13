039a19214193b8f47f884f725f4eb77f9ee73449
bonachea <dobonachea@lbl.gov>
2021-04-13 19:59:19 -0400

    Bump development version to 2021.3.3

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20210301L
+#define UPCXX_VERSION 20210303L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
