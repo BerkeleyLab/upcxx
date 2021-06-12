d59ba436baa5e1a64ff224f90d76ba926e310da4
Amir Kamil <akamil@umich.edu>
2021-06-11 23:06:46 -0400

    Disable use of __builtin_launder on Intel compilers.

--- utils/config/upcxx/builtin_launder.sh
+++ utils/config/upcxx/builtin_launder.sh
@@ -17,11 +17,17 @@ int main() {
     A *a = ::new(&b) A;
     a->x = 3;
     const int j = __builtin_launder(reinterpret_cast<A*>(&b))->x;
+    #if __INTEL_COMPILER
+    // Intel compiler does not appear to properly support __builtin_launder
+    // see issue 481 and PR 358 for details
+    return 1;
+    #endif
     return !(i == -1 && j == 3);
 }
 _EOF
 
-if eval ${GASNET_CXX} ${GASNET_CXXCPPFLAGS} ${GASNET_CXXFLAGS} -o conftest conftest.cpp &> /dev/null; then
+TEST="(${GASNET_CXX} ${GASNET_CXXCPPFLAGS} ${GASNET_CXXFLAGS} -o conftest conftest.cpp && ./conftest)"
+if eval $TEST &> /dev/null; then
   echo '#define UPCXX_HAVE___BUILTIN_LAUNDER 1'
 else
   echo '#undef UPCXX_HAVE___BUILTIN_LAUNDER'
