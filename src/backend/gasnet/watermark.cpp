e1cc7b1f0fb965bd2819fa6333dc44d00df20a21
Amir Kamil <akamil@umich.edu>
2021-07-03 17:25:57 -0400

    Rename UPCXX_GIT_VERSION with UPCXXI prefix.

--- src/backend/gasnet/watermark.cpp
+++ src/backend/gasnet/watermark.cpp
@@ -26,16 +26,16 @@ GASNETT_IDENT(UPCXXI_IdentString_LibraryVersion, "$UPCXXLibraryVersion: " _STRIN
 #endif
 GASNETT_IDENT(UPCXXI_IdentString_SpecVersion, "$UPCXXSpecVersion: " _STRINGIFY(UPCXX_SPEC_VERSION) " $");
 
-// UPCXX_GIT_VERSION is defined by the Makefile for this object on the command line,
+// UPCXXI_GIT_VERSION is defined by the Makefile for this object on the command line,
 // when built from a git checkout (with a .git directory) and the git describe command succeeds.
 // Otherwise, the git_version.h header is included, which is empty in releases but in snapshots is
-// overwritten with a file containing a #define of UPCXX_GIT_VERSION by the snapshot-building script.
-// If none of these sources provide a UPCXX_GIT_VERSION (eg in a release) this ident string is omitted.
-#ifndef UPCXX_GIT_VERSION
+// overwritten with a file containing a #define of UPCXXI_GIT_VERSION by the snapshot-building script.
+// If none of these sources provide a UPCXXI_GIT_VERSION (eg in a release) this ident string is omitted.
+#ifndef UPCXXI_GIT_VERSION
 #include <upcxx/git_version.h>
 #endif
-#ifdef  UPCXX_GIT_VERSION
-GASNETT_IDENT(UPCXXI_IdentString_GitVersion, "$UPCXXGitVersion: " _STRINGIFY(UPCXX_GIT_VERSION) " $");
+#ifdef  UPCXXI_GIT_VERSION
+GASNETT_IDENT(UPCXXI_IdentString_GitVersion, "$UPCXXGitVersion: " _STRINGIFY(UPCXXI_GIT_VERSION) " $");
 #endif
 
 #if UPCXXI_BACKEND_GASNET_SEQ
