dd9fd3399fa2650715194a63f62fc522277cada2
bonachea <dobonachea@lbl.gov>
2021-09-26 19:50:12 -0700

    persona-example: update to match current best-practice
    * Accept LPC argument as const reference.
      This instance was apparently missed in the PG PR 20 pass before last
      release.
    
    * Don't pass `source_cx::as_buffered()` to `upcxx::rpc()`
      This has always been redundant in practice, and as of 2021.3.0
      was explicitly specified as redundant.
    
    * Remove the `thread_barrier` stall which oversynchronizes threads and
      was already demonstrated in the prior example (persona-example-rputs.cpp).
      Replace it with a `upcxx::discharge()` call; this is the minimum
      required for correctness, allows worker threads to exit much sooner,
      and allows us to highlight this important best-practice use of
      discharge
    
    * Fix a bad indent

--- example/prog-guide/dmap-lpc.hpp
+++ example/prog-guide/dmap-lpc.hpp
@@ -30,8 +30,8 @@ public:
   void find(const std::string &key, upcxx::persona & persona, Func func) {
     // the value returned by the RPC is passed as an argument to the LPC 
     // used in the completion object
-    auto cx = upcxx::source_cx::as_buffered() | upcxx::operation_cx::as_lpc(persona,func);
-    upcxx::rpc(get_target_rank(key),cx,
+    auto cx = upcxx::operation_cx::as_lpc(persona,func);
+    upcxx::rpc(get_target_rank(key), cx,
         // lambda to find the key in the local map
         [](dobj_map_t &lmap, const std::string &key) -> std::string {
           auto elem = lmap->find(key);
--- example/prog-guide/persona-example.cpp
+++ example/prog-guide/persona-example.cpp
@@ -43,7 +43,6 @@ int main(int argc, char *argv[])
   thread * threads[num_threads];
   // declare an agreed upon persona for the progress thread
   upcxx::persona progress_persona;
-  atomic<int> thread_barrier(0);
   int lpc_count = N;
   // liberate the master persona to allow the progress thread to use it
   upcxx::liberate_master_persona();
@@ -59,8 +58,6 @@ int main(int argc, char *argv[])
         upcxx::progress();
       }
       cout<<"Progress thread on process "<<upcxx::rank_me()<<" is done"<<endl; 
-      // unlock the other threads
-      thread_barrier += 1;
       });
   // launch multiple threads to perform find operations
   for (int tid=0; tid<num_threads; tid++) {
@@ -73,17 +70,14 @@ int main(int argc, char *argv[])
           string key = to_string((upcxx::rank_me() + 1) % upcxx::rank_n()) + ":" + to_string(i);
           // attach callback, which itself runs a LPC on progress_persona on completion
           dmap.find(key, progress_persona, 
-              [key,&lpc_count](string val) {
+              [key,&lpc_count](const string &val) {
                 assert(val == key);
                 lpc_count--;
               });
         }
-        // block here until the progress thread has executed all RPCs and LPCs
-        while(thread_barrier.load(memory_order_acquire) != 1){
-          sched_yield();
-          upcxx::progress();
-        }
-        });
+        // discharge outgoing find operations before thread exit:
+        upcxx::discharge(); 
+    });
   }
 
   // wait until all threads are done
