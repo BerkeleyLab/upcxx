aea8a16d618bdb0a31f08d4f8da6c6e88e2a15e4
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-01-19 16:00:49 -0800

    configure: factor use of preprocessor
    This commit factors the logic to invoke the pre-preprocessor stage of
    the C or C++ compiler to extract the expansion of a given expression,
    validating it against a bash regex.

--- utils/system-checks.sh
+++ utils/system-checks.sh
@@ -26,6 +26,84 @@ sys_info() {
     ) fi
 }
 
+# Run C or C++ preprocessor to extract an expression matching a bash regex
+#
+# Arguments:
+#   compiler: literal 'CC' or 'CXX'
+#       expr: string to be expanded between a pair of delimiters
+#      regex: bash regular expression used to extract the actual result
+#      flags: (optional) extra compiler flags (such as -I and -D)
+#   includes: (optional) string to be expanded first in the source file
+#
+# The return value is normally the result of the bash regex operation:
+#   0 == success (results of match in ${UPCXX_REMATCH[@]})
+#   1 == failure
+#   2 == bash rejected the regex as invalid
+#   3 == other error detected
+#
+# Notes:
+#  + The `expr` should be a valid C expression in a context like
+#       identifier1 + expr + identifier2
+#    Anything else risks triggering undefined behavior in the preprocessor.
+#  + Leading and trailing whitespace in the expansion of `expr` will be lost.
+#  + Whitespace internal to the expansion of `expr` might change.
+#  + The `regex` should not be so greedy that it matches the delimiters.
+#    For instance `.*` would be unsafe, but `(.*)` would be safe (assuming
+#    that the parentheses are included in the `expr`).
+#  + _CONCAT{,3,4,5,6}() and _STRINGIFY are available
+#
+cpp_extract_expr() {
+    case $1 in
+         CC) local suffix=c   cmd="$CC  $CFLAGS   $4 -E";;
+        CXX) local suffix=cpp cmd="$CXX $CXXFLAGS $4 -E";;
+          *) echo Internal error; exit 3;;
+    esac
+    local expr="$2"
+    local regex="$3"
+    local headers="$5"
+
+    local conftest="conftest.$suffix"
+    trap "rm -f $conftest" RETURN
+
+    local token1='_6VTuuw1pJg0eJR4d_'
+    local token2='_5tf48oGssuj89WUi_'
+    rm -f $conftest
+    cat >$conftest <<_EOF
+      $headers
+      #ifndef _CONCAT
+        #define _CONCAT_HELPER(a,b) a ## b
+        #define _CONCAT(a,b) _CONCAT_HELPER(a,b)
+      #endif
+      #ifndef _CONCAT3
+        #define _CONCAT3(a,b,c) _CONCAT(a,_CONCAT(b,c))
+      #endif
+      #ifndef _CONCAT4
+        #define _CONCAT4(a,b,c,d) _CONCAT(a,_CONCAT3(b,c,d))
+      #endif
+      #ifndef _CONCAT5
+        #define _CONCAT5(a,b,c,d,e) _CONCAT(a,_CONCAT4(b,c,d,e))
+      #endif
+      #ifndef _CONCAT6
+        #define _CONCAT6(a,b,c,d,e,f) _CONCAT(a,_CONCAT5(b,c,d,e,f))
+      #endif
+      #ifndef _STRINGIFY
+        #define _STRINGIFY_HELPER(x) #x
+        #define _STRINGIFY(x) _STRINGIFY_HELPER(x)
+      #endif
+      ${token1}+${expr}+${token2}
+_EOF
+
+    # Strip our delimiters and any whitespace introduced by the preprocessor
+    local space=$' \t\n\v\f\r' # [:space:] == space, tab, newline, vertical tab, form feed, carriage return
+    local delim="[$space]*\+[$space]*"
+    if ! [[ $(eval $cmd $conftest) =~ ${token1}${delim}(${regex})${delim}${token2} ]]; then
+      return $?
+    fi
+
+    UPCXX_REMATCH=("${BASH_REMATCH[@]:1}")  # "shifted" to remove full match with our delimeters
+    return 0
+}
+
 # For probing for lowest acceptable (for its libstdc++) g++ version.
 # These are defaults, which may be overridden per-platform (such as Cray XC).
 MIN_GNU_MAJOR=6
@@ -42,28 +120,15 @@ MIN_GNU_STRING='6.4'
 #   1 - identified too-low version
 #   other - failed to identify version
 check_gnu_version() {
-    case $1 in
-         CC) local suffix=c   compiler="${2:-$CC $CFLAGS}" ;;
-        CXX) local suffix=cpp compiler="${2:-$CXX $CXXFLAGS}";;
-          *) echo Internal error; exit 1;;
-    esac
-    trap "rm -f conftest.$suffix" RETURN
-    local TOKEN1='_MKkiiTv4jDk8Tmw6_'
-    local TOKEN2='_SDPECv3TjARP7xiZ_'
-    cat >conftest.$suffix <<_EOF
-      #undef  _REPORT
-      #undef  _REPORT_HELPER
-      #define _REPORT(a,b,c) _REPORT_HELPER(a,b,c)
-      #define _REPORT_HELPER(a,b,c) $TOKEN1 ## a ## _ ## b ## _ ## c ## $TOKEN2
-      _REPORT(__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__)
-_EOF
-    if ! [[ $(eval $compiler -E conftest.$suffix) =~ ${TOKEN1}([0-9]+)_([0-9]+)_([0-9]+)${TOKEN2} ]]; then
+    local cpp_expr='_CONCAT6(v,__GNUC__,_,__GNUC_MINOR__,_,__GNUC_PATCHLEVEL__)'
+    local bash_re='v([0-9]+)_([0-9]+)_([0-9]+)'
+    if ! cpp_extract_expr $1 "$cpp_expr" "$bash_re"; then
         echo "ERROR: regex match failed probing \$$1 for GNUC version"
         return 2
     fi
-    gnu_major=${BASH_REMATCH[1]}
-    gnu_minor=${BASH_REMATCH[2]}
-    gnu_patch=${BASH_REMATCH[3]}
+    gnu_major=${UPCXX_REMATCH[1]}
+    gnu_minor=${UPCXX_REMATCH[2]}
+    gnu_patch=${UPCXX_REMATCH[3]}
     gnu_version="$gnu_major.$gnu_minor.$gnu_patch"
     return $(( (    gnu_major*1000000 +     gnu_minor*1000 +     gnu_patch) <
                (MIN_GNU_MAJOR*1000000 + MIN_GNU_MINOR*1000 + MIN_GNU_PATCH) ))
@@ -243,27 +308,12 @@ compile_check() {
     local DETAIL_LOG=config-detail.log
     rm -f $DETAIL_LOG
     # check if we need to inject -std=c++11 flag
-    trap "rm -f conftest-std.cpp" RETURN
-    local TOKEN1='_reYBrfDyyWZ76wwb_'
-    local TOKEN2='_unnBZgmdLe3ADU4F_'
-    cat >conftest-std.cpp <<_EOF
-      #undef  _REPORT
-      #undef  _REPORT_HELPER
-      #define _REPORT(a) _REPORT_HELPER(a)
-      #define _REPORT_HELPER(a) $TOKEN1 ## a ## $TOKEN2
-      #if __cplusplus < 201103L
-      _REPORT(1)
-      #else
-      _REPORT(0)
-      #endif
-_EOF
-    if ! [[ $(eval $CXX $CXXFLAGS -E conftest-std.cpp) =~ ${TOKEN1}([0-9]+)${TOKEN2} ]]; then
-        echo "ERROR: regex match failed probing \$$1 for C++ standard version"
+    if ! cpp_extract_expr CXX '__cplusplus' '([1-9][0-9]+)[lL]?'; then
+        echo "ERROR: regex match failed probing \$CXX for C++ standard version"
         return 4
     fi
-    local cxx_pre11=${BASH_REMATCH[1]}
     local CXXSTDFLAG=
-    if [[ $cxx_pre11 -ne 0 ]]; then
+    if (( ${UPCXX_REMATCH[1]} < 201103 )); then
         CXXSTDFLAG="-std=c++11"
     fi
     # check C compilation
