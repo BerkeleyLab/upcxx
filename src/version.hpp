1f0d85af401962d38b69f075d9a894c7c2f33230
bonachea <dobonachea@lbl.gov>
2022-02-03 14:25:35 -0800

    Bump development version to 2021.9.3
    This specifically corresponds to the availability of cuda_device::device_n()
    and hip_device

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20210901L
+#define UPCXX_VERSION 20210903L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
