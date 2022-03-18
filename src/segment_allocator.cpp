BerkeleyLab/upcxx

commit 14e291319e07df2e1c60101e6642583362496b19

Make cuda_device, hip_device, and device_allocator MoveAssignable.

src/segment_allocator.cpp
@@ -54,6 +54,24 @@ segment_allocator::segment_allocator(segment_allocator &&that):
 }
 
 GASNETT_COLD
+segment_allocator& segment_allocator::operator=(segment_allocator &&that) {
+  seg_base_ = that.seg_base_;
+  holes_by_size_ = std::move(that.holes_by_size_);
+  hunks_by_begin_ = std::move(that.hunks_by_begin_);
+
+  that.seg_base_ = 0;
+
+  this->endpost_ = that.endpost_;
+  that.endpost_.begin = 0;
+  that.endpost_.prev = nullptr;
+
+  if(this->endpost_.prev != nullptr)
+    this->endpost_.prev->next = &this->endpost_;
+
+  return *this;
+}
+
+GASNETT_COLD
 segment_allocator::~segment_allocator() {
   block *b = endpost_.prev;
   while(b != nullptr) {

src/cuda.hpp
@@ -35,6 +35,7 @@ namespace upcxx {
     cuda_device(id_type device_id = invalid_device_id);
     cuda_device(cuda_device const&) = delete;
     cuda_device(cuda_device&& other) : gpu_device(std::move(other)) {}
+    cuda_device& operator=(cuda_device&& other) = default;
 
     static id_type device_n();
 
@@ -65,6 +66,7 @@ namespace upcxx {
       device_allocator_core() {}
       device_allocator_core(cuda_device &dev, void *base, std::size_t size);
       device_allocator_core(device_allocator_core&&) = default;
+      device_allocator_core& operator=(device_allocator_core&&) = default;
       ~device_allocator_core() { release(); }
       void release();
     };

src/device_allocator.hpp
@@ -33,6 +33,21 @@ namespace upcxx {
           other.heap_idx_ = -1; // deactivate
         }
       }
+      device_allocator_base& operator=(device_allocator_base&& other) {
+        UPCXX_ASSERT(
+          !is_active(),
+          "Move assignment is only allowed an an inactive device allocator"
+        );
+        heap_idx_ = other.heap_idx_;
+        seg_ = std::move(other.seg_);
+        if (heap_idx_ >= 0) {
+          backend::heap_state *hs = backend::heap_state::get(heap_idx_);
+          UPCXX_ASSERT(hs->alloc_base == &other);
+          hs->alloc_base = this; // update registration
+          other.heap_idx_ = -1; // deactivate
+        }
+        return *this;
+      }
 
       inline bool is_active() const { return heap_idx_ >= 0; }
     };
@@ -153,6 +168,23 @@ namespace upcxx {
         )
       ), implicit_device(that.implicit_device) {
       that.implicit_device = nullptr;
+    }
+
+    device_allocator& operator=(device_allocator &&that) {
+      // base class move assign
+      heap_allocator::operator=(std::move(that));
+      detail::device_allocator_core<Device>::operator=(
+        static_cast<detail::device_allocator_core<Device>&&>(
+          ( UPCXXI_ASSERT_MASTER_HELD_IFSEQ(), // required to ensure thread-safety wrt allocate
+            // use comma operator to create a temporary lock_guard surrounding
+            // the invocation of our base class's move ctor
+            std::lock_guard<detail::par_mutex>(that.lock_),
+            that)
+        )
+      );
+      implicit_device = that.implicit_device;
+      that.implicit_device = nullptr;
+      return *this;
     }
 
     void destroy(upcxx::entry_barrier eb = entry_barrier::user) override {

src/device_fwd.hpp
@@ -170,6 +170,19 @@ class device {
       other.heap_idx_ = -1; // deactivate
     }
   }
+  device& operator=(device&& other) {
+    UPCXX_ASSERT(heap_idx_ == -1,
+                 "Move assignment is only allowed an an inactive device");
+    UPCXX_ASSERT(kind_ == other.kind_);
+    heap_idx_ = other.heap_idx_;
+    if (heap_idx_ >= 0) {
+      backend::heap_state *hs = backend::heap_state::get(heap_idx_);
+      UPCXX_ASSERT(hs->device_base == &other);
+      hs->device_base = this; // update registration
+      other.heap_idx_ = -1; // deactivate
+    }
+    return *this;
+  }
 
   template<typename Device>
   static typename Device::id_type heap_idx_to_device_id(int heap_idx);
@@ -214,6 +227,12 @@ class gpu_device : public detail::device {
   gpu_device(gpu_device&& other) :
     device(std::move(other)), device_id_(other.device_id_) {
     other.device_id_ = invalid_device_id;
+  }
+  gpu_device& operator=(gpu_device&& other) {
+    device::operator=(std::move(other));
+    device_id_ = other.device_id_;
+    other.device_id_ = invalid_device_id;
+    return *this;
   }
 
   // computes Device::default_alignment<T> without a static type T

src/hip.hpp
@@ -35,6 +35,7 @@ namespace upcxx {
     hip_device(id_type device_id = invalid_device_id);
     hip_device(hip_device const&) = delete;
     hip_device(hip_device&& other) : gpu_device(std::move(other)) {}
+    hip_device& operator=(hip_device&& other) = default;
 
     static id_type device_n();
 
@@ -65,6 +66,7 @@ namespace upcxx {
       device_allocator_core() {}
       device_allocator_core(hip_device &dev, void *base, std::size_t size);
       device_allocator_core(device_allocator_core&&) = default;
+      device_allocator_core& operator=(device_allocator_core&&) = default;
       ~device_allocator_core() { release(); }
       void release();
     };

src/segment_allocator.hpp
@@ -47,6 +47,7 @@ namespace detail {
     segment_allocator(void *segment_base, std::size_t segment_size);
     segment_allocator(segment_allocator const&) = delete;
     segment_allocator(segment_allocator &&that);
+    segment_allocator& operator=(segment_allocator &&that);
     ~segment_allocator();
 
     std::pair<void*,std::size_t> segment_range() const {

commit b36d02b65e4781018faeacef099187acda159ba7

FIXUP: Make cuda_device, hip_device, and device_allocator MoveAssignable.
This fixes a small memory leak that was introduced in 14e29131

The simples fix in this case is to avoid the dynamic allocation when
creating an empty `segment_allocator`.

Also add enforcement that `segment_allocator`move assignment only
overwrites an empty `segment_allocator` object.

src/segment_allocator.cpp
@@ -22,16 +22,17 @@ namespace {
 }
 
 GASNETT_COLD
-segment_allocator::segment_allocator(void *segment_base, size_t segment_size) {
-  this->seg_base_ = reinterpret_cast<uintptr_t>(segment_base);
-  
+segment_allocator::segment_allocator(void *segment_base, size_t segment_size) :
+  seg_base_(reinterpret_cast<uintptr_t>(segment_base)),
+  endpost_{0,segment_size,nullptr,nullptr} {
+
+  if (!segment_size) return; // empty segment
+
   block *big_hole = new block{
     /*is_hole*/1,
     /*begin*/0,
     /*next, prev*/nullptr, &endpost_
   };
-  this->endpost_.begin = segment_size;
-  this->endpost_.is_hole = 0;
   this->endpost_.prev = big_hole;
   
   insert_hole_by_size(holes_by_size_, big_hole, segment_size);
@@ -45,6 +46,8 @@ segment_allocator::segment_allocator(segment_allocator &&that):
 
 GASNETT_COLD
 segment_allocator& segment_allocator::operator=(segment_allocator &&that) {
+  UPCXX_ASSERT(!this->seg_base_ && !this->endpost_.begin,
+               "Move assignment is only allowed an an inactive segment_allocator");
   seg_base_ = that.seg_base_;
   holes_by_size_ = std::move(that.holes_by_size_);
   hunks_by_begin_ = std::move(that.hunks_by_begin_);