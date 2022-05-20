e7ffa0a8905eae4b6463ab49d652f1c78260a1f3
bonachea <dobonachea@lbl.gov>
2022-05-20 15:33:30 -0700

    issue 549: Adjust UPCXXI_ISSUE_485_SLOW_THE_ALWAYS to allow per-TU modification of `future_header_result<>::always`
    Resolves issue #549

--- src/future/core.cpp
+++ src/future/core.cpp
@@ -13,20 +13,18 @@ using upcxx::detail::future_body_proxy_;
 template<typename ...T>
 using future_header_result = upcxx::detail::future_header_result<T...>;
 
-#if UPCXXI_ISSUE_485_SLOW_THE_ALWAYS
 // IMPORTANT NOTE: We rely on this to be constant initialized so that
 // functions like make_future() work correctly during dynamic
 // initialization. The initializers here must all be constant
 // expressions.
-const future_header future_header_result<>::the_always = {
+const future_header future_header_result<>::the_always_slow = {
   /*ref_n_*/-1,
   /*status_*/upcxx::detail::future_header::status_ready,
   /*sucs_head_*/nullptr,
   {/*result_*/const_cast<future_header*>(
-                &upcxx::detail::future_header_result<>::the_always
+                &upcxx::detail::future_header_result<>::the_always_slow
               )}
 };
-#endif
 
 #if UPCXXI_PROMISE_VTABLE_HACK
 const detail::promise_vtable detail::the_promise_vtable<>::vtbl{
