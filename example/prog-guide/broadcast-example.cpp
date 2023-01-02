744a2b1c037ee7c01c69866a0a9e92e4396c9537
bonachea <dobonachea@lbl.gov>
2023-01-02 13:58:01 -0800

    example/prog-guide: Improve the broadcast example
    It now demonstrates how to efficiently broadcast a `std::vector`
    whose size is initially known only by the root rank.
    
    Resolves issue #575

--- example/prog-guide/broadcast-example.cpp
+++ example/prog-guide/broadcast-example.cpp
@@ -10,30 +10,35 @@ int main(int argc, char *argv[])
   upcxx::init();
 
 //SNIPPET
+std::vector<int> buffer;
+size_t elem_count{};
+if (upcxx::rank_me() == 0) {
+  elem_count = 42; // initially only rank 0 knows the element count
+}
+// launch a scalar broadcast of element count from rank 0
+upcxx::future<size_t> fut = upcxx::broadcast(elem_count, 0);
 
-int my_rank = upcxx::rank_me();
-// launch a first broadcast operation from rank 0
-upcxx::future<int> fut = upcxx::broadcast(my_rank, 0);
+// do some overlapped work like preparing buffer for next broadcast
+if (upcxx::rank_me() == 0) {
+  buffer.reserve(elem_count);
+  for (size_t i = 0; i < elem_count; i++)
+    buffer[i] = (int)i;
+}
 
-// do some overlapped work like preparing a buffer for another broadcast
-std::vector<int> buffer(10);
-if ( upcxx::rank_me() == 0 )
-  for (int i = 0; i< buffer.size(); i++)
-    buffer[i] = i;
+elem_count = fut.wait(); // complete first broadcast
+buffer.reserve(elem_count); // non-zero ranks allocate vector space
 
-// launch a second broadcast operation from rank 0
-upcxx::future<> fut_bulk = upcxx::broadcast( buffer.data(), buffer.size(), 0); 
-
-// wait for the result from the first broadcast
-int bcast_rank = fut.wait();
-assert(bcast_rank == 0);
+// launch a bulk broadcast of element data from rank 0
+upcxx::future<> fut_bulk = upcxx::broadcast( buffer.data(), elem_count, 0); 
 
 // wait until the second broadcast is complete
 fut_bulk.wait();
-for (int i = 0; i< buffer.size(); i++)
-  assert(buffer[i] == i);
 
+// consume elements
+for (size_t i = 0; i < buffer.size(); i++)
+  UPCXX_ASSERT_ALWAYS(buffer[i] == (int)i);
 //SNIPPET
+
 
   upcxx::barrier();
   if (!upcxx::rank_me()) cout << "SUCCESS" << endl;
