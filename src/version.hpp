e7956bed8e67edebd4ddea3d2343e9716f908622
bonachea <dobonachea@lbl.gov>
2023-02-16 12:20:45 -0800

    Bump development version to 2022.9.5
    Corresponds to the first availability of gpu_device::kind_info()

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20220903L
+#define UPCXX_VERSION 20220905L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
