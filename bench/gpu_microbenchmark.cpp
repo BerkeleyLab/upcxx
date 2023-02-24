af062e566b3c9eecb657f621d5d843cfde12a0a8
bonachea <dobonachea@lbl.gov>
2023-02-23 19:52:52 -0800

    bench/gpu_microbenchmark: disable bidirectional tests for single-rank jobs
    In single-rank jobs the gpu_microbenchmark flood tests were actually
    identical in behavior between uni- and bi-directional modes, but the
    latter was over-reporting achieved bi-directional bandwidth by a factor of 2.
    
    This defect only affected the degenerate case of single-rank jobs.
    With two or more processes, bidirectional tests have always included payload
    transfers flowing in both directions between pairs of processes, leading
    to correctly reported total per-process bi-directional bandwidth results.

--- bench/gpu_microbenchmark.cpp
+++ bench/gpu_microbenchmark.cpp
@@ -611,6 +611,10 @@ int do_main(int argc, char **argv) {
        if (!run_uni && !run_bi) {
          run_uni = run_bi = true;
        }
+       if (upcxx::rank_n() == 1) {
+         std::cerr << "WARNING: Single-rank job, bi-directional tests disabled\n" << std::flush;
+         run_bi = false;
+       }
        if (!run_put && !run_get) {
          run_put = run_get = true;
        }
