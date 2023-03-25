08cdfa6e0a2fcd53d5812b1c147d0e696c759f42
bonachea <dobonachea@lbl.gov>
2023-03-24 21:16:14 -0400

    Set package version 2023.3.0 for release

--- src/version.hpp
+++ src/version.hpp
@@ -3,11 +3,11 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20220907L
+#define UPCXX_VERSION 20230300L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
-#define UPCXX_SPEC_VERSION 20220900L
+#define UPCXX_SPEC_VERSION 20230300L
 
 namespace upcxx {
   long release_version();
